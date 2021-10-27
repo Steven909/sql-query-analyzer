65080c1a923ae65e082e2337da21f40339523464
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-10-27 18:36:55 +0100

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+// A utility application which a user can enter a query and have it run against a local database
+// and look for ways to make it more efficient.
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/CommunicationDB.java
@@ -0,0 +1,57 @@
+import java.sql.*;
+
+public class CommunicationDB {
+
+    private static Connection getDBConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        try {
+            String dbURL = "jdbc:sqlite:dvd:sqlite";
+
+            connection = DriverManager.getConnection(dbURL);
+            return connection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
+    private static void retrieve() throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultset = null;
+        String query = "SELECT * FROM collection;";
+        try {
+            connection = getDBConnection();
+            statement = connection.createStatement();
+            System.out.println(query);
+            resultset = statement.executeQuery(query);
+            while (resultset.next()) {
+                System.out.println(resultset.getString("ID"));
+                System.out.println(resultset.getString("Title"));
+                System.out.println(resultset.getString("Genre"));
+                System.out.println(resultset.getString("Year"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (resultset != null) {
+                resultset.close();
+            }
+            if (
+                    statement != null) {
+                statement.close();
+            }
+            if (
+                    connection != null) {
+                connection.close();
+            }
+        }
+    }
+}
